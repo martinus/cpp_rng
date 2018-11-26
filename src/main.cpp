a33f1d715e5c6e1570ff2527f27188ac558d5cde
Martin Ankerl <martin.ankerl@gmail.com>
2018-11-26 05:19:12 +0100

    mixin improvements

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,48 @@ namespace detail {
 // Mixin class to extend plain random number generator engines
 template <typename Engine> class Extras {
 public:
+	static constexpr uint64_t(min)() { return (std::numeric_limits<uint64_t>::min)(); }
+	static constexpr uint64_t(max)() { return (std::numeric_limits<uint64_t>::max)(); }
+
 	double real01()
 	{
-		static_assert(sizeof(typename Engine::result_type) == 8);
-
 		union {
 			uint64_t i;
 			double d;
 		} x;
-		x.i = (UINT64_C(0x3ff) << 52) | (engine()() >> 12);
+		x.i = (UINT64_C(0x3ff) << 52) | (next() >> 12);
 		return x.d - 1.0;
 	}
 
 	double real_between(double min_val, double max_val) { return (max_val - min_val) * real01(); }
 
+	uint64_t bounded(uint64_t boundExcluded)
+	{
+#ifdef __SIZEOF_INT128__
+		using u128 = unsigned __int128;
+		return static_cast<uint64_t>(
+			(static_cast<u128>(boundExcluded) * static_cast<u128>(next())) >> 64);
+#else
+		// see https://github.com/imneme/pcg-c-basic/blob/master/pcg_basic.c#L79
+		uint64_t const threshold = (0 - bound) % bound;
+
+		for (;;) {
+			const uint64_t r = next();
+			if (r >= threshold) {
+				return r % bound;
+			}
+		}
+#endif
+	}
+
 private:
-	// Since this is the CRTP, it's safe to do a static cast.
-	Engine& engine() { return *static_cast<Engine*>(this); }
+	// Since this is the CRTP, it's safe to do a static cast. Calls the engines operator()() to get
+	// the next random number.
+	uint64_t next()
+	{
+		static_assert(std::is_same<typename Engine::result_type, uint64_t>::value, "");
+		return (*static_cast<Engine*>(this))();
+	}
 
 	// Ensure correctly correct derivation of the CRTP:
 	// make all ctor's private, but only Engine a friend
@@ -74,6 +99,7 @@ private:
 };
 
 // based on http://xoshiro.di.unimi.it/xoshiro256starstar.c
+// other C++ implementation: https://github.com/groshart/random/blob/master/xoshiro256_rand.hpp
 class xoshiro256starstar : public detail::Extras<xoshiro256starstar> {
 public:
 	using result_type = uint64_t;
@@ -122,5 +148,7 @@ int main(int, char**)
 	xoshiro256starstar rng(123);
 	for (size_t i = 0; i < 10; ++i) {
 		std::cout << rng.real01() << std::endl;
+		std::cout << rng.bounded(32) << std::endl;
 	}
+	std::cout << xoshiro256starstar::max() << std::endl;
 }
